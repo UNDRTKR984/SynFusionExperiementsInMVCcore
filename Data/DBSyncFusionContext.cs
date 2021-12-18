8bcc26ae28305415a90b7727ca8efe66068f2565
Matt Vogel <63422957+UNDRTKR984@users.noreply.github.com>
2021-12-17 19:22:20 -0500

    Uploaded files
    Uploaded files for viewing

---
+++ Data/DBSyncFusionContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DBSyncFusion.Models;
+
+namespace DBSyncFusion.Data
+{
+    public class DBSyncFusionContext : DbContext
+    {
+        public DBSyncFusionContext (DbContextOptions<DBSyncFusionContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DBSyncFusion.Models.Person> Person { get; set; }
+    }
+}
